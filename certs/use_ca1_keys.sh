a1d03016d7c14cb478e8048b941c77b6e683dad5
tjamir <tjamir@gmail.com>
2021-03-22 15:06:55 -0300

    Adding sample certificates

---
+++ certs/use_ca1_keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf client.*
+
+ln -s client1.crt client.crt
+ln -s client1.key client.key
+
---
+++ certs/use_ca2_keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf client.*
+
+ln -s client2.crt client.crt
+ln -s client2.key client.key
+
